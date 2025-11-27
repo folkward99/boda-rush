9a2f43e3efb2a586571a717f856c71a6189e3ed4
folkward99 <dfolkward@gmail.com>
2025-11-27 22:08:17 +0300

    Add Bike1 prefab meta file with initial configuration

--- Assets/Highway Racer/Scripts/HR_GamePlayHandler.cs
+++ Assets/Highway Racer/Scripts/HR_GamePlayHandler.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0ba6721b26e3cb2458eeb0d19f6a5aa904ff75863314b5851e061cd97144cd9b
-size 5989
+oid sha256:cc1586984b6b8f083997ab272a1413d2f311a1d030e06eddad6bc1b2b726849b
+size 6391
--- Assets/TestBike.cs
+++ Assets/TestBike.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5f647d9c53245fa7bf9ac13ad525cfb91fc88e03bf1d5fefba7701cc177f986e
-size 4651
+oid sha256:106408caffdd7edbc289d3f03a04b3e2549e46d18b7ac7fc26097b54a52bc378
+size 4674
