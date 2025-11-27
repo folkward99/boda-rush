f029010f9efa16975b2003fd584392e7fb8e5c86
folkward99 <dfolkward@gmail.com>
2025-11-27 23:21:12 +0300

    Update metadata for Bike1 prefab and TestBike script to reflect new sizes and hashes

--- Assets/TestBike.cs
+++ Assets/TestBike.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:42101075d7f2e795f05afb5364fe5e2cdcb7b03d5618ab6184f8501c4597776c
-size 8363
+oid sha256:e57bcdad0560c2291b97a8b9ef90a81b9b98c6527d3d14ba6edf592b205cb7ef
+size 12327
